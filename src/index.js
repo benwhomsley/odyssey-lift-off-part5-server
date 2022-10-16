320bb1452c449735b9cac139b7b4f9c7611bcdb2
Ben Whomsley <ben.whomsley@thebeansgroup.com>
2022-10-16 14:08:10 +0100

    add PORT number

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const TrackAPI = require('./datasources/track-api');
 
-async function startApolloServer(typeDefs, resolvers) {
+async function startApolloServer (typeDefs, resolvers) {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
@@ -14,7 +14,7 @@ async function startApolloServer(typeDefs, resolvers) {
     },
   });
 
-  const { url, port } = await server.listen();
+  const { url, port } = await server.listen({ port: process.env.PORT || 4000 });
   console.log(`
       🚀  Server is running
       🔉  Listening on port ${port}
